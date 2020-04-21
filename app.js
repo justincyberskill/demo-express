3b55c7422dc783ca830fd7fc8634ca844670032a
Justin Jr <minh-thong.lu@cyberskill.tech>
2020-04-22 02:57:22 +0700

    feat/get-method-with-query-string

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ app.set("views", "./views");
 app.get("/", (_, res) => {
   res.send("Nice to meet you, ExpressJS");
 });
+const todoItems = ["Eat", "Sleep", "Walk"];
 app.get("/todos", (_, res) => {
-  res.render("todo");
+  res.render("todo", { items: todoItems });
+});
+app.get("/todos/search", (req, res) => {
+  const { t } = req.query;
+  const matchedTodo = todoItems.filter((item) => {
+    return item.indexOf(t) !== -1;
+  });
+
+  res.render("todo", { items: matchedTodo, searchValue: t });
 });
 
 const port = process.env.PORT || 3000;
